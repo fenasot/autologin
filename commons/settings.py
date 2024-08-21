c1f65fa7effbf4ec5cf494642b7cd42496ea3b53
fenasot <fenahave@gmail.com>
2024-08-21 20:28:23 +0800

    add: 新增ini讀取class與文件夾架構

--- commons/settings.py
+++ commons/settings.py
@@ -7,35 +7,10 @@ from configparser import ConfigParser
 from selenium.webdriver.common.by import By
 
 
-"""此文件用來存放全域變數與設定資料"""
+"""此文件用來存放全域變數與其他設定資料"""
 
 
-'''設定共用路徑'''
-p = Path()
-p_cwd = p.cwd()
-
-# PACKAGE_PATH = Path.PurePath()  # 當前檔案路徑
-PACKAGE_PATH = str(p_cwd.parent)  # 專案頂層路徑
-
-COMMONS_PATH = str(p.cwd())  # commons 路徑
-CONFIGS_PATH = str(PurePath(PACKAGE_PATH, 'configs'))
-DATAS_PATH = str(PurePath(PACKAGE_PATH, 'datas'))
-LOGS_PATH = str(PurePath(PACKAGE_PATH, 'logs'))
-SRC_PATH = str(PurePath(PACKAGE_PATH, 'src'))
-# SRC_PATH = str(PurePath(PACKAGE_PATH, 'src'))
-# PACKAGE_PATH = os.getcwd()
-
-
-def aaaaa():
-    print(PACKAGE_PATH)
-    print('ssssss', p.cwd())
-    print(CONFIGS_PATH)
-    # raise PACKAGE_PATH
-
-
-aaaaa()
-
-
+'''全域變數'''
 # DOM 元素取得判斷
 BY_TYPE = {
     "id": By.ID,
@@ -45,20 +20,51 @@ BY_TYPE = {
     "xpath": By.XPATH
 }
 
-ini = ConfigParser()
+
+'''設定共用路徑'''
+p = Path()
+p_cwd = p.cwd()
+
+# PATH_PACKAGE = Path.PurePath()  # 當前檔案路徑
+PATH_PACKAGE = str(p_cwd.parent)  # 專案頂層路徑
+
+PATH_COMMONS = str(p.cwd())  # commons 路徑
+PATH_CONFIGS = str(PurePath(PATH_PACKAGE, 'configs'))
+PATH_DATAS = str(PurePath(PATH_PACKAGE, 'datas'))
+PATH_LOGS = str(PurePath(PATH_PACKAGE, 'logs'))
+PATH_SRC = str(PurePath(PATH_PACKAGE, 'src'))
+# PATH_PACKAGE = os.getcwd()
 
 
 class Readini:
-    """ini文件讀取"""
+    """ini 文件讀取設定"""
 
     def __init__(self) -> None:
-        self.ini = ini.read_file(PACKAGE_PATH)
+        self.ini = ConfigParser()
+        self.ini.read_file(f'{PATH_CONFIGS}\\configs.ini')
 
-    def get(self, section: str, name: str):
-        """取得ini"""
-        # ini.get(name, section, name)
-        ini.get(section, name)
+    def get(self, section: str, option: str) -> None:
+        """取得ini的option"""
+        # self.__check_section(section)
+        self.__check_option(section, option)
+        self.ini.get(section, option)
 
-    def set_file(self, file_name: str):
-        """設定讀取的ini文件"""
-        ini.read_file(file_name)
+    def set(self, section: str, option: str, value: str) -> None:
+        """鍵入ini的option (若沒有section，將自動新增section)"""
+        if self.ini.has_section(section) is False:
+            self.ini.add_section(section)
+        self.ini.set(section, option, value)
+
+    def set_read_file(self, source: str) -> None:
+        """改變讀取的ini文件"""
+        self.ini.read_file(source)
+
+    def __check_section(self, section: str) -> None:
+        """確認section是否存在"""
+        if self.ini.has_section(section) is False:
+            raise AttributeError(f'ini section ({section}) not found.')
+
+    def __check_option(self, section: str, option: str) -> None:
+        """確認option是否存在"""
+        if self.ini.has_option(section, option) is False:
+            raise AttributeError(f'ini option ({option}) not found.')
